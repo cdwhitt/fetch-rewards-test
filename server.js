65f417e1bad5a76cf500c23799e382243a894a91
cdwhitt <cwhitt91@gmail.com>
2020-10-08 11:45:54 -0400

    Create sorting algorithm in server

--- server.js
+++ server.js
@@ -3,24 +3,48 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = process.env.PORT || 5000
 const axios = require("axios")
-// const cors = require("cors")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(cors())
 
 app.get("/api/fetch-rewards-data", async (req, res) => {
+	//Fetch JSON data
 	const { data } = await axios.get(
 		"https://fetch-hiring.s3.amazonaws.com/hiring.json"
 	)
 
+	//Filter out items with "" or null name
 	const filteredData = data.filter(
 		(item) => item.name !== "" && item.name !== null
 	)
 
-	console.log(filteredData, "FILTERED DATA")
+	//Sort items first by listId then by id
+	const sortedData = filteredData.sort(function (a, b) {
+		const listIdA = a.listId
+		const listIdB = b.listId
 
-	res.send(filteredData)
+		//Sort first on listId
+		if (listIdA > listIdB) {
+			return 1
+		} else if (listIdA < listIdB) {
+			return -1
+		} else {
+			// If the listIds are the same, do a nested sort on id
+			const idA = a.id
+			const idB = b.id
+
+			if (idA > idB) {
+				return 1
+			} else if (idA < idB) {
+				return -1
+			} else {
+				return 0
+			}
+		}
+	})
+
+	//Send sortedData to front end
+	res.send(sortedData)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import "./App.css"
+import { Table } from "semantic-ui-react"
 const App = () => {
 	const [list, setList] = useState([])
+	// const [loading, setLoading] = useState(false)
+	// const [currentPage, setCurrentPage] = useState(1)
+	// const [listItemsPerPage, setListItemsPerPage] = useState(10)
 
 	useEffect(() => {
 		const callApiAndSetData = async () => {
 			const { data } = await axios.get("/api/fetch-rewards-data")
-
 			setList(data)
 		}
-
 		callApiAndSetData()
 	}, [])
 
-	const listNodes = list.map((listItem) => {
-		return <li key={listItem.id}>{listItem.name}</li>
+	const tableRows = list.map(({ id, listId, name }) => {
+		return (
+			<Table.Row key={name}>
+				<Table.Cell>{listId}</Table.Cell>
+				<Table.Cell>{name}</Table.Cell>
+				<Table.Cell>{id}</Table.Cell>
+			</Table.Row>
+		)
 	})
 
 	return (
-		<div className="App">
-			<ul>{listNodes}</ul>
-		</div>
+		<Table celled padded>
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell>List ID</Table.HeaderCell>
+					<Table.HeaderCell>Name</Table.HeaderCell>
+					<Table.HeaderCell>ID</Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>{tableRows}</Table.Body>
+		</Table>
 	)
 }
 
